20fd4b92f96b82025b41ef2ac15a5171ace44249
Joaquin Rivera Padron <joahking@gmail.com>
2009-04-17 11:57:46 +0200

    added installations notes for cucumber mode

--- cucumber-mode.el
+++ cucumber-mode.el
@@ -1,20 +1,26 @@
 ;; cucumber.el -- Emacs mode for editing plain text user stories
 ;;
 ;; Copyright (C) 2008 Michael Klishin
-;; 
-;; This program is free software; you can redistribute it and/or 
-;; modify it under the terms of the GNU General Public License 
-;; as published by the Free Software Foundation; either version 2 
-;; of the License, or (at your option) any later version. 
-;;  
-;; This program is distributed in the hope that it will be useful, 
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of 
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-;; GNU General Public License for more details. 
-;;  
-;; You should have received a copy of the GNU General Public License 
-;; along with this program; if not, write to the Free Software 
-;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. 
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;; add this to your .emacs to load the mode
+;; (add-to-list 'load-path "~/.emacs.d/elisp/cucumber-mode")
+;; ;; and load it
+;; (autoload 'cucumber-mode "cucumber-mode" "Mode for editing cucumber files" t)
+;; (add-to-list 'feature-mode '("\.feature$" . cucumber-mode))
 
 (eval-when-compile (require 'cl))
 
