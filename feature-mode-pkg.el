d4f5580eb8bd2cdca21bd1203cac6efbc957720d
Michael Klishin <michael@clojurewerkz.org>
2014-11-21 18:30:31 +0000

    Bump version to 0.5.0-dev

--- feature-mode-pkg.el
+++ feature-mode-pkg.el
@@ -1,1 +1,1 @@
-(define-package "feature-mode" "0.4" "Major mode for editing Gherkin (i.e. Cucumber) user stories")
+(define-package "feature-mode" "0.5.0-dev" "Major mode for editing Gherkin (i.e. Cucumber) user stories")
