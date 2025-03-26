5c92811676d0201efd38c03a5bbf5fa470d4a375
Stéphane Maniaci <stephane.maniaci@gmail.com>
2025-03-26 18:11:58 +0100

    features: move the specs away from "Given an empty file"
    Which from a quick investigation yields files within `tmp/aruba`
    folder, whilst the regular `Given a file` yields a file at the current
    working dir which is what we want here.

--- features/step_definitions/emacs_steps.rb
+++ features/step_definitions/emacs_steps.rb
@@ -2,8 +2,9 @@ require 'aruba/cucumber'
 require 'debug'
 
 When(/^I invoke (\S+) on (\S+)$/) do | function, feature_file |
-    cmd = %<emacs --batch -l ../../feature-mode.el #{feature_file} --eval '(progn (feature-mode) (#{function}) (set-buffer "*compilation*") (princ (buffer-string)))'>
-    step %(I run `#{cmd}`)
+  cmd = %<emacs --batch -l org -l org-table -l ./feature-mode.el #{feature_file} --eval '(progn (feature-mode) (#{function}) (set-buffer "*compilation*") (princ (buffer-string)))'>
+
+  step %(I run `#{cmd}`)
 end
 
 Given('a debugger') do
