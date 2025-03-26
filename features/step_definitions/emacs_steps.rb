9997006481a9c87c8d85bc73821b2103391c0f58
Stéphane Maniaci <stephane.maniaci@gmail.com>
2025-03-26 18:11:26 +0100

    gem: add the debug gem to figure things out if needed

--- features/step_definitions/emacs_steps.rb
+++ features/step_definitions/emacs_steps.rb
@@ -1,6 +1,11 @@
 require 'aruba/cucumber'
+require 'debug'
 
 When(/^I invoke (\S+) on (\S+)$/) do | function, feature_file |
     cmd = %<emacs --batch -l ../../feature-mode.el #{feature_file} --eval '(progn (feature-mode) (#{function}) (set-buffer "*compilation*") (princ (buffer-string)))'>
     step %(I run `#{cmd}`)
+end
+
+Given('a debugger') do
+  debugger # rubocop:disable Lint/Debugger
 end
